186d5b4ae281cde4a5db3c070f381303f41d732d
Joao Danilo <joaodanilo1992@gmail.com>
2022-11-30 06:34:14 -0400

    Solve keystore2 crash

--- jaodroid.mk
+++ jaodroid.mk
@@ -48,3 +48,9 @@ PRODUCT_PACKAGES += \
   android.hardware.fastboot@1.0-impl-mock \
   fastbootd
 # ================= END =================
+
+# ================= keystore2 =================
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+# ================= END =================
