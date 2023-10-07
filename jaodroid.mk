359c69612ff1226bc2585cf4c399708b64a5f345
Joao Danilo <joaodanilo1992@gmail.com>
2023-10-07 15:07:15 -0400

    Modifications to porting for Android 14

--- jaodroid.mk
+++ jaodroid.mk
@@ -1,5 +1,6 @@
 $(call  inherit-product,  $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call  inherit-product,  $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # ================= Production Options =================
 PRODUCT_NAME            :=  jaodroid
@@ -10,10 +11,10 @@ PRODUCT_MANUFACTURER    :=  jao
 # ================= END =================
 
 # ================= Local Variables =================
-KERNEL_VERSION    :=  5.4
-LOCAL_DTB         :=  device/casa/jaodroid/blackbox/${KERNEL_VERSION}
+KERNEL_VERSION    :=  6.1-TS050
+LOCAL_DTB         :=  device/casa/jaodroid/blackbox/kernel/${KERNEL_VERSION}
 DTBO_UNSIGNED     :=  dtbo-unsigned.img
-LOCAL_KERNEL      :=  device/casa/jaodroid/blackbox/${KERNEL_VERSION}/Image.lz4
+LOCAL_KERNEL      :=  device/casa/jaodroid/blackbox/kernel/${KERNEL_VERSION}/Image.lz4
 HARDWARE_NAME     :=  jaodroid
 TARGET_BOARD_PLATFORM := yukawa
 # ================= END =================
